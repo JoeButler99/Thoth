bcfba8a493a46bd6fa5ae1e5c31c89de685a7f3a
Joe Butler <joe_butler99@hotmail.com>
2014-03-02 14:22:50 +0000

    Refactoring test cases - TestFunction.h

--- test/TestFunction.h
+++ test/TestFunction.h
@@ -42,8 +42,16 @@
 
 class TestFunction : public CppUnit::TestFixture {
 public:
-	TestFunction(){}
+	TestFunction() : stackLevel(0) {}
 	virtual ~TestFunction(){}
+	int stackLevel;
+	double rpnStack[RPN_STACK_LIMIT];
+
+	void setUp() {
+		stackLevel = 0;
+		rpnStack = {0};
+
+	}
 
 	static CppUnit::Test *suite() {
 		CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite("TestFunction");
@@ -83,6 +91,9 @@ public:
 		return suiteOfTests;
 	}
 protected:
+	void setStack(double * vars,)
+
+
 	 void testCustomConstructor() {
 		std::cerr << "Function:\t\t" <<  __func__ << std::endl;
 		Function f("Fadd",2,0,rpnAdd);
@@ -142,7 +153,7 @@ protected:
 
 	 void testFdivide() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("Fdivide",2,3);
+	 	Function f("Fdivide",2,3,rpnDivide);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 0.83333333 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - -0.833333333 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -0.5 ) < 0.000001);
@@ -154,7 +165,7 @@ protected:
 
 	 void testFpercentme() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-		Function f("Fpercentme",2,3);
+		Function f("Fpercentme",2,3,rpnPercentMe);
 		CPPUNIT_ASSERT(fabs(f.Fpercentme(20) - 0.2 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fpercentme(0.01) - 0.0001 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fpercentme(-0.01) - -0.0001 ) < 0.000001);
@@ -164,7 +175,7 @@ protected:
 
 	 void testFunpercentme() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-		Function f("Funpercentme",2,3);
+		Function f("Funpercentme",2,3,rpnUnPercentMe);
 		CPPUNIT_ASSERT(fabs(f.Funpercentme(20) - 2000 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Funpercentme(0.01) - 1 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Funpercentme(-0.01) - -1 ) < 0.000001);
@@ -174,7 +185,7 @@ protected:
 
 	 void testFsquared() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("Fpercentme",2,3);
+	 	Function f("Fpercentme",2,3,rpnSquare);
 		CPPUNIT_ASSERT(fabs(f.Fsquared(20) - 400 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fsquared(0.01) - 0.0001 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fsquared(-0.01) - 0.0001 ) < 0.000001);
@@ -183,7 +194,7 @@ protected:
 
 	 void testFPC_change() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("FPC_change",2,6);
+	 	Function f("FPC_change",2,6,rpnPercentChange);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 20 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - -220 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -100 ) < 0.000001);
@@ -195,7 +206,7 @@ protected:
 
 	 void testFPC_of() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("FPC_of",2,7);
+	 	Function f("FPC_of",2,7,rpnPercentOf);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 83.33333333 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - -83.33333333 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -0.5 ) < 0.000001);
@@ -207,7 +218,7 @@ protected:
 
 	 void testFAVG_2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("AVG 2",2,8);
+	 	Function f("AVG 2",2,8,rpnAvgOf2);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 11 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - 1 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -0.25 ) < 0.000001);
@@ -219,7 +230,7 @@ protected:
 
 	 void testFmin_2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("min 2",2,9);
+	 	Function f("min 2",2,9,rpnMinOf2);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 10 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - -10 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -0.5 ) < 0.000001);
@@ -231,7 +242,7 @@ protected:
 
 	 void testFmax_2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnMaxOf2);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 12 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - 12 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - -0 ) < 0.000001);
@@ -243,7 +254,7 @@ protected:
 
 	 void testFAVG_4() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnAvgOf4);
 		CPPUNIT_ASSERT(fabs(f.FAVG_4(0,1,2,3) - 1.5) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAVG_4(10,-3,0,-7) - 0) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAVG_4(20,-30,10,-10) - -2.5) < 0.000001);
@@ -253,7 +264,7 @@ protected:
 
 	 void testFmin_4() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnMinOf4);
 		CPPUNIT_ASSERT(fabs(f.Fmin_4(0,1,2,3) - 0) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fmin_4(10,-3,0,-7) - -7) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fmin_4(20,-30,10,-10) - -30) < 0.000001);
@@ -263,7 +274,7 @@ protected:
 
 	 void testFmax_4() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnMaxOf4);
 		CPPUNIT_ASSERT(fabs(f.Fmax_4(0,1,2,3) - 3) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fmax_4(10,-3,0,-7) - 10) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fmax_4(20,-30,10,-10) - 20) < 0.000001);
@@ -273,7 +284,7 @@ protected:
 
 	 void testFInt_Me() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnToInt);
 	 	CPPUNIT_ASSERT(fabs(f.FInt_Me(0) - 0) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FInt_Me(0.6) - 1) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FInt_Me(-0.7) - -1) < 0.000001);
@@ -283,7 +294,7 @@ protected:
 
 	 void testFsquare_rt() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnSquareRoot);
 	 	CPPUNIT_ASSERT(fabs(f.Fsquare_rt(0) - 0) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.Fsquare_rt(0.6) - 0.7745966692) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.Fsquare_rt(-0.7) - 0.8366600265) < 0.000001);
@@ -293,7 +304,7 @@ protected:
 
 	 void testFif_4_1() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnIfV1);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_1(0,1,2,3) - 3) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_1(10,-3,0,-7) - 0) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_1(20,-30,10,-10) - 10) < 0.000001);
@@ -303,7 +314,7 @@ protected:
 
 	 void testFif_4_2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnIfV2);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_2(0,1,2,3) - 1) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_2(10,-3,0,-7) - -7) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.Fif_4_2(20,-30,10,-10) - -10) < 0.000001);
@@ -313,7 +324,7 @@ protected:
 
 	 void testFSum() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnSum4);
 		CPPUNIT_ASSERT(fabs(f.FSum(0,1,2,3) - 6) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FSum(10,-3,0,-7) - 0) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FSum(20,-30,10,-10) - -10) < 0.000001);
@@ -323,7 +334,7 @@ protected:
 
 	 void testFAbsSum2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,20);
+	 	Function f("max 2",2,20,rpnAbsSum2);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 22 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - 22 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - 0.5 ) < 0.000001);
@@ -335,7 +346,7 @@ protected:
 
 	 void testFAbsSum4() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,10);
+	 	Function f("max 2",2,10,rpnAbsSum4);
 		CPPUNIT_ASSERT(fabs(f.FAbsSum4(0,1,2,3) - 6) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAbsSum4(10,-3,0,-7) - 20) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAbsSum4(20,-30,10,-10) - 70) < 0.000001);
@@ -345,7 +356,7 @@ protected:
 
 	 void testFAbsMax2() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,22);
+	 	Function f("max 2",2,22,rpnAbsMax2);
 		CPPUNIT_ASSERT(fabs(f.action(10,12) - 12 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-10,12) - 12 ) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.action(-0.5,0) - 0.5 ) < 0.000001);
@@ -357,7 +368,7 @@ protected:
 
 	 void testFAbsMax4() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,22);
+	 	Function f("max 2",2,22,rpnAbsMax4);
 		CPPUNIT_ASSERT(fabs(f.FAbsMax4(0,1,2,3) - 3) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAbsMax4(10,-3,0,-7) - 10) < 0.000001);
 		CPPUNIT_ASSERT(fabs(f.FAbsMax4(20,-30,10,-10) - 30) < 0.000001);
@@ -367,7 +378,7 @@ protected:
 
 	 void testFlipDouble() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,22);
+	 	Function f("max 2",2,22,rpnNegate);
 	 	CPPUNIT_ASSERT(fabs(f.FlipDouble(0) - 0) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FlipDouble(0.6) - -0.6) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FlipDouble(-0.7) - .7) < 0.000001);
@@ -377,7 +388,7 @@ protected:
 
 	 void testFRaiseE() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,22);
+	 	Function f("max 2",2,22,rpnRaiseE);
 	 	CPPUNIT_ASSERT(fabs(f.FRaiseE(0) - 0) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FRaiseE(0.6) - 0.2494322333) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FRaiseE(-0.7) - -0.379256312) < 0.000001);
@@ -387,7 +398,7 @@ protected:
 
 	 void testFLnE() {
 	 	std::cerr << "Function:\t\t" <<  __func__ << std::endl;
-	 	Function f("max 2",2,22);
+	 	Function f("max 2",2,22,rpnLnE);
 	 	CPPUNIT_ASSERT(fabs(f.FLnE(0) - 0) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FLnE(0.6) - -0.510825624) < 0.000001);
 	 	CPPUNIT_ASSERT(fabs(f.FLnE(-0.7) - 0.356675) < 0.000001);
